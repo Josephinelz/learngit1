06989efe40b94e5875e5b6e42c804b829e545b4a
Li Zhuo <1250641751@qq.com>
2018-05-23 20:07:27 +0800

    删除了部分内容

--- readme.cs
+++ readme.cs
@@ -1,5 +1,5 @@
 //E:\lz\软件备份\多频接收机\20180516\MultiFreqGNSS_V2.0_INS - 副本 
             //关于开环的测试还存在问题，先解决将速度误差减小的问题。 
             string genFileDir = @"E:\lz\测试结果文件\20180520\1608_0515_IMU_Simulator_vehicle_76725M_1ms_8Hz_INSMoveDoppler_DCPLL_9Sats_prn4test\";
-            string filename = @"180515024951";
+            string filename =
 			
